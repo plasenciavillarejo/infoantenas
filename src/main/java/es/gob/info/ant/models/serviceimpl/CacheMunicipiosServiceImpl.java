a2424d301497fcea3ae3ff1307012b880f7c839c
Andres1291 <a.nieto@ibermatica.com>
2024-05-13 20:33:44 +0200

    [Andres] [cache municipios]

--- src/main/java/es/gob/info/ant/models/serviceimpl/CacheMunicipiosServiceImpl.java
+++ src/main/java/es/gob/info/ant/models/serviceimpl/CacheMunicipiosServiceImpl.java
@@ -1,10 +1,9 @@
 package es.gob.info.ant.models.serviceimpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +18,7 @@ public class CacheMunicipiosServiceImpl implements ICacheMunicipiosService {
 	private ICacheMunicipiosDao municipiosDao;
 	
 	@Override
+	@Cacheable(value = "municipiosCacheados")
 	@Transactional(readOnly = true)
 	public Slice<CacheMunicipiosDto> listarMunicipios(Pageable pageable, Long codProvincia) {
 		return municipiosDao.listarMunicipios(pageable, codProvincia);
