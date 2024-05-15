8be8b72bc4d25dd72d7fe25d90caa3ea71a8f8e3
Jose Plasencia <jose.plasencia@externos.correo.gob.es>
2024-05-15 10:01:51 +0200

    [Plasencia]

--- src/main/java/es/gob/info/ant/serviceimpl/ProvinciasServiceImpl.java
+++ src/main/java/es/gob/info/ant/serviceimpl/ProvinciasServiceImpl.java
@@ -1,8 +1,6 @@
 package es.gob.info.ant.serviceimpl;
 
-import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
 
 import org.springframework.stereotype.Service;
 
@@ -13,68 +11,62 @@ public class ProvinciasServiceImpl implements IProvinciasService {
 
 	@Override
 	public String siglasProvincias(String nombreProvincia) {
-		Map<String, String> listadoProvincias = new HashMap<>();
-		listadoProvincias.put("VI", "Araba/Álava");
-		listadoProvincias.put("AB", "Albacete");
-		listadoProvincias.put("A", "Alicante/Alacant");
-		listadoProvincias.put("AL",	"Almería");
-		listadoProvincias.put("O", "Asturias");
-		listadoProvincias.put("AV", "Ávila");
-		listadoProvincias.put("BA", "Badajoz");
-		listadoProvincias.put("IB","Illes Balears");
-		listadoProvincias.put("B","Barcelona");
-		listadoProvincias.put("BU","Burgos");
-		listadoProvincias.put("CC","Cáceres");
-		listadoProvincias.put("CA","Cádiz");
-		listadoProvincias.put("CS","Castellón/Castelló");
-		listadoProvincias.put("CR","Ciudad Real");
-		listadoProvincias.put("CO","Córdoba");
-		listadoProvincias.put("C","A Coruña");
-		listadoProvincias.put("CU","Cuenca");
-		listadoProvincias.put("GI","Girona");
-		listadoProvincias.put("GR","Granada");
-		listadoProvincias.put("GU","Guadalajara");
-		listadoProvincias.put("SS","Gipuzkoa");
-		listadoProvincias.put("H","Huelva");
-		listadoProvincias.put("HU","Huesca");
-		listadoProvincias.put("J","Jaén");
-		listadoProvincias.put("LE","León");
-		listadoProvincias.put("L","Lleida");
-		listadoProvincias.put("LO","La Rioja");
-		listadoProvincias.put("LU","Lugo");
-		listadoProvincias.put("MA","Málaga");
-		listadoProvincias.put("MU","Murcia");
-		listadoProvincias.put("NA","Navarra");
-		listadoProvincias.put("OU","Ourense");
-		listadoProvincias.put("P","Palencia");
-		listadoProvincias.put("GC","Las Palmas");
-		listadoProvincias.put("PO","Pontevedra");
-		listadoProvincias.put("SA","Salamanca");
-		listadoProvincias.put("TF","Santa Cruz de Tenerife");
-		listadoProvincias.put("S","Cantabria");
-		listadoProvincias.put("SG","Segovia");
-		listadoProvincias.put("SE","Sevilla");
-		listadoProvincias.put("SO","Soria");
-		listadoProvincias.put("T","Tarragona");
-		listadoProvincias.put("TE","Teruel");
-		listadoProvincias.put("TO","Toledo");
-		listadoProvincias.put("V","Valencia/València");
-		listadoProvincias.put("VA","Valladolid");
-		listadoProvincias.put("BI","Bizkaia");
-		listadoProvincias.put("ZA","Zamora");
-		listadoProvincias.put("Z","Zaragoza");
-		listadoProvincias.put("CE","Ceuta");
-		listadoProvincias.put("ML","Melilla");
+		Map<String, String> listadoProvincias = Map.ofEntries(
+		Map.entry("VI", "Araba/Álava"),
+		Map.entry("AB", "Albacete"),
+		Map.entry("A", "Alicante/Alacant"),
+		Map.entry("AL",	"Almería"),
+		Map.entry("O", "Asturias"),
+		Map.entry("AV", "Ávila"),
+		Map.entry("BA", "Badajoz"),
+		Map.entry("IB","Illes Balears"),
+		Map.entry("B","Barcelona"),
+		Map.entry("BU","Burgos"),
+		Map.entry("CC","Cáceres"),
+		Map.entry("CA","Cádiz"),
+		Map.entry("CS","Castellón/Castelló"),
+		Map.entry("CR","Ciudad Real"),
+		Map.entry("CO","Córdoba"),
+		Map.entry("C","A Coruña"),
+		Map.entry("CU","Cuenca"),
+		Map.entry("GI","Girona"),
+		Map.entry("GR","Granada"),
+		Map.entry("GU","Guadalajara"),
+		Map.entry("SS","Gipuzkoa"),
+		Map.entry("H","Huelva"),
+		Map.entry("HU","Huesca"),
+		Map.entry("J","Jaén"),
+		Map.entry("LE","León"),
+		Map.entry("L","Lleida"),
+		Map.entry("LO","La Rioja"),
+		Map.entry("LU","Lugo"),
+		Map.entry("MA","Málaga"),
+		Map.entry("MU","Murcia"),
+		Map.entry("NA","Navarra"),
+		Map.entry("OU","Ourense"),
+		Map.entry("P","Palencia"),
+		Map.entry("GC","Las Palmas"),
+		Map.entry("PO","Pontevedra"),
+		Map.entry("SA","Salamanca"),
+		Map.entry("TF","Santa Cruz de Tenerife"),
+		Map.entry("S","Cantabria"),
+		Map.entry("SG","Segovia"),
+		Map.entry("SE","Sevilla"),
+		Map.entry("SO","Soria"),
+		Map.entry("T","Tarragona"),
+		Map.entry("TE","Teruel"),
+		Map.entry("TO","Toledo"),
+		Map.entry("V","Valencia/València"),
+		Map.entry("VA","Valladolid"),
+		Map.entry("BI","Bizkaia"),
+		Map.entry("ZA","Zamora"),
+		Map.entry("Z","Zaragoza"),
+		Map.entry("CE","Ceuta"),
+		Map.entry("ML","Melilla"));
 		
-		AtomicReference<String> provinciaRecuperada = new AtomicReference<>();
-				
-		listadoProvincias.forEach((key, value) -> {
-			if(value.equalsIgnoreCase(nombreProvincia)) {
-				provinciaRecuperada.set(key);
-			}
-		});
-		
-		return provinciaRecuperada.get();
+		return 	listadoProvincias.entrySet().stream().filter(value -> value.getValue().equalsIgnoreCase(nombreProvincia))
+				.map(Map.Entry::getKey)
+				.findFirst().orElse("No se ha encontrado la Provincia a buscar");		
 	}
 
 }
