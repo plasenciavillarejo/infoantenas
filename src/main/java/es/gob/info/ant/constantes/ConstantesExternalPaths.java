fa6c5d01908d0cdd203476f031194cc7c719146c
Jose Plasencia Villarejo <plasenciavillarejo@gmail.com>
2024-05-26 13:12:29 +0200

    [Plasencia]-[Docker]

--- src/main/java/es/gob/info/ant/constantes/ConstantesExternalPaths.java
+++ src/main/java/es/gob/info/ant/constantes/ConstantesExternalPaths.java
@@ -5,5 +5,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class ConstantesExternalPaths {
 
-	public static final String INFOANTENAS_CONFIGURATION_PROPERTIES_PATH="file:${infoantenas.configpath}/infoant.properties";
+	//public static final String INFOANTENAS_CONFIGURATION_PROPERTIES_PATH="file:${infoantenas.configpath}/infoant.properties";
+	
+	// Trabajando con docker
+	public static final String INFOANTENAS_CONFIGURATION_PROPERTIES_PATH="file:/config/infoant.properties";
 }
+
+
